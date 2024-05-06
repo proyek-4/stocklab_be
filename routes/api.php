e5893b4da891bd5695196f30316705380350f70c
ghifariwandanaa <ghifariwandana@gmail.com>
2024-05-06 08:24:49 +0700

    add : controller, routes for login,register

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,18 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StockController;
 use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\LoginController;
+
 //Register dan Login
 // Route::apiResource('registers', RegisterController::class)->only('store');
-// Route::post('/login', [LoginController::class,'login']);
+Route::post('/register', RegisterController::class)->name('register');
+Route::post('/login', LoginController::class)->name('login');
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
 
 Route::apiResource('stocks', StockController::class)->only(['index', 'show', 'destroy', 'store']);
 Route::post('/stocks/update/{id}', [StockController::class, 'update']);
 Route::get('/stocks/edit/{id}', [StockController::class, 'edit']);
-Route::post('/register', RegisterController::class)->name('register');
